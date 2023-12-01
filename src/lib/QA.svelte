f11b21fb986374af6b13e7d4926d3289896669f5
Mike Dielen <mdielen@thefactory.nl>
2023-11-30 16:49:21 -0800

    now with files

---
+++ src/lib/QA.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	let active = false;
+	export let question = 'question';
+</script>
+
+<div class="transition-all">
+	<label class="cursor-pointer transition-all">
+		<input type="checkbox" bind:checked={active} class="hidden" />
+		<h3>
+			<span class={clsx(active && 'inline-flex rotate-90', 'transition-all')}>⮚</span>
+			{question}
+		</h3></label
+	>
+	<div class={clsx('prose px-2', !active && 'hidden')}>
+		<slot><p>Answer</p></slot>
+	</div>
+</div>
