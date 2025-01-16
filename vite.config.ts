defd3d25bf2fafa2f2519d7e6ba4fb2e699a17b3
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-01-16 16:12:46 +0100

    Copy configs from freshly made app + fix deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { isoImport } from 'vite-plugin-iso-import';
 
 export default defineConfig({
-	plugins: [sveltekit(), isoImport()],
+	plugins: [sveltekit()],
 	test: {
 		// Exclude /tests/ as it contains playwright tests
 		include: ['**/__tests__/**/*.?(c|m)[jt]s?(x)', 'src/**/?(*.){test,spec}.?(c|m)[jt]s?(x)']
