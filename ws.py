117705ab1ad8ce891e4db2d5ff7ab1a325b8fda6
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-01-21 11:32:39 +0100

    Cache forever

--- ws.py
+++ ws.py
@@ -27,8 +27,8 @@ CORS(app)
 
 cache = Cache(config={
     'CACHE_TYPE': 'FileSystemCache', 
-    # 'CACHE_TYPE': 'NullCache', // to disable caching
-    'CACHE_DEFAULT_TIMEOUT': 36000, 
+    # 'CACHE_TYPE': 'NullCache', # to disable caching
+    'CACHE_DEFAULT_TIMEOUT': 0, # cache never expires
     'CACHE_DIR': './cache'
 })
 cache.init_app(app)
