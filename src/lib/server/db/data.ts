4081833279e13a98fe70ff59a0157faec98c136c
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-01-23 13:39:11 +0100

    Hardcode paths for deployment

--- src/lib/server/db/data.ts
+++ src/lib/server/db/data.ts
@@ -1,6 +1,7 @@
 import { open_borders } from './borders';
 
-export const dataDir = 'data'
+// export const dataDir = 'data'
+export const dataDir = '/data/DataUpdate_02_2024'
 
 const bordersPath = dataDir + '/ne_110m_admin_0_countries.geojson';
 
