3163adfae1052db77ec0769b815f851dbb1c6877
Chantal <chantalwue@gmail.com>
2025-01-27 10:59:12 +0100

    Formatting to pass linting check

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -83,8 +83,8 @@ export function pathwayQueryFromSearchParams(
 }
 
 export const API_URL = dev
-       ? import.meta.env.CABE_API_URL ?? 'http://127.0.0.1:5000'
-       : process.env.CABE_API_URL ?? 'http://127.0.0.1:5000'
+	? import.meta.env.CABE_API_URL ?? 'http://127.0.0.1:5000'
+	: process.env.CABE_API_URL ?? 'http://127.0.0.1:5000';
 
 async function getJSON(path: string, myfetch = fetch) {
 	let url = `${API_URL}${path}`;
