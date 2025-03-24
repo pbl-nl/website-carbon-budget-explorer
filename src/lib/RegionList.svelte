d0435daf3678ca05a5e2639543f271637c72ca4c
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-03-24 16:43:29 +0100

    Have regions and countries section on country popup

--- src/lib/RegionList.svelte
+++ src/lib/RegionList.svelte
@@ -10,10 +10,23 @@
 </script>
 
 <!-- TODO add filter by name and filter by non-country (aka ISO2===null) -->
+<h1 class="text-lg">Regions</h1>
 <div class=" grid w-full grid-flow-row grid-cols-5" aria-label="Regions">
 	{#each regions as region}
-		<a aria-label={region.iso3} href={`/regions/${region.iso3}${$page.url.search}`}>
-			{region.name}
-		</a>
+		{#if 'countries' in region}
+			<a aria-label={region.iso3} href={`/regions/${region.iso3}${$page.url.search}`}>
+				{region.name}
+			</a>
+		{/if}
+	{/each}
+</div>
+<h1 class="text-lg">Countries</h1>
+<div class=" grid w-full grid-flow-row grid-cols-5" aria-label="Regions">
+	{#each regions as region}
+		{#if !('countries' in region)}
+			<a aria-label={region.iso3} href={`/regions/${region.iso3}${$page.url.search}`}>
+				{region.name}
+			</a>
+		{/if}
 	{/each}
 </div>
