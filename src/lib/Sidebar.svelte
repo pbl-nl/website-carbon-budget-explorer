b2b071640aaf6e10e87753256fdd9c7108d9c6a9
Chantal <chantalwue@gmail.com>
2025-05-22 10:39:58 +0200

    run lint/format

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -6,9 +6,12 @@
 		children?: import('svelte').Snippet;
 	}
 
-	let { children, className}: Props = $props();
+	let { children, className }: Props = $props();
 </script>
 
-<div id="sidebar" class={clsx("flex h-full w-1/3 flex-col place-content-stretch gap-4 2xl:w-1/4", className)}>
+<div
+	id="sidebar"
+	class={clsx('flex h-full w-1/3 flex-col place-content-stretch gap-4 2xl:w-1/4', className)}
+>
 	{@render children?.()}
 </div>
